8d74a66a05ebbebfc95fbbf4b351f12ba141aff8
ArtyWhit <julwer1k@gmail.com>
2024-05-20 21:58:33 +0300

    create a project: todo app

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,41 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import classNames from 'classnames';
+/* eslint-disable jsx-a11y/control-has-associated-label */
 import React from 'react';
-import './App.scss';
-
-interface Props {
-  onClick: () => void;
-  children: React.ReactNode;
-}
-
-export const Provider: React.FC<Props> = React.memo(({ onClick, children }) => (
-  <button type="button" onClick={onClick}>
-    {children}
-  </button>
-));
+import * as todosService from './api/todos';
+import { ErrorNotification } from './components/ErrorNotification';
+import { Header } from './components/Header';
+import { TodoList } from './components/TodoList';
+import { Footer } from './components/Footer';
+import { useTodos } from './lib/TodosContext';
+import { UserWarning } from './UserWarning';
 
 export const App: React.FC = () => {
+  const { todos, isHeaderFocused } = useTodos();
+
+  if (!todosService.USER_ID) {
+    return <UserWarning />;
+  }
+
   return (
-    <div className="starter">
-      <Provider onClick={() => ({})}>TodoList</Provider>
+    <div className="todoapp">
+      <h1
+        className={classNames('todoapp__title', {
+          'todoapp__title--focused': isHeaderFocused,
+        })}
+      >
+        todos
+      </h1>
+
+      <div className="todoapp__content">
+        <Header />
+
+        <TodoList />
+
+        {!!todos.length && <Footer />}
+      </div>
+
+      <ErrorNotification />
     </div>
   );
 };
